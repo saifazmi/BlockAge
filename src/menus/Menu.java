4dd761099e5ea09df789f756b7913a9ab8e95959
Paul Popa <paul.popa1995@yahoo.ro>
2016-02-12 16:53:53 +0000

    lets hope for the best

---
+++ src/menus/Menu.java
@@ -0,0 +1,12 @@
+package menus;
+import javafx.scene.Scene;
+
+/**
+ * Interface for the menus in which we put the width and height for all the menus
+ * @author paulp
+ */
+public interface Menu {
+	public static final int WIDTH = 1280;
+	public static final int HEIGHT = 720;
+	public Scene getScene();
+}
---
+++ src/menus/OptionsMenu.java
@@ -0,0 +1,99 @@
+package menus;
+
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import sceneElements.ButtonProperties;
+import sceneElements.ElementsHandler;
+import sceneElements.LabelProperties;
+
+public class OptionsMenu implements Menu {
+	
+	
+	public static Button yesButtonH, yesButtonS, noButtonH, noButtonS, backButton = null;
+	public static Image hintsImage, soundImage, onImage, onImageHovered, offImage, offImageHovered, backImage, backImageHovered = null;
+	public static int spaceBetweenText = 100;
+	public static int spaceBetweenImgH = 50;
+	public static Pane optionsMenuPane = null;
+	
+
+	private Scene optionsMenuScene = null;
+	private Label hintsLabel, soundLabel = null;
+	private LabelProperties l = null;
+	private ButtonProperties b = null;
+
+	public OptionsMenu() {
+			initialiseScene();
+	}
+	/**
+	 * Declaring the elements which will be placed on the scene
+	 */
+	public void declareElements() {
+		yesButtonH = new Button();
+		noButtonH = new Button();
+		yesButtonS = new Button();
+		noButtonS = new Button();
+		backButton = new Button();
+		
+		hintsLabel= new Label();
+		soundLabel = new Label();
+		
+		optionsMenuPane = new Pane();
+		l = new LabelProperties();
+		b = new ButtonProperties();
+		
+		onImage = new Image(getClass().getResourceAsStream("images/on_button.png"));
+		onImageHovered = new Image(getClass().getResourceAsStream("images/on_button_hovered.png"));
+		
+		offImage = new Image(getClass().getResourceAsStream("images/off_button.png"));
+		offImageHovered = new Image(getClass().getResourceAsStream("images/off_button_hovered.png"));
+		
+		hintsImage = new Image(getClass().getResourceAsStream("images/hints_label.png"));
+		soundImage = new Image(getClass().getResourceAsStream("images/sound_label.png"));
+		
+		backImage = new Image(getClass().getResourceAsStream("images/back_button.png"));
+		backImageHovered = new Image(getClass().getResourceAsStream("images/back_button.hovered.png"));
+	}
+	/**
+	 * Initialise the scene for the options menu and what will be inside the scene
+	 */
+	public void initialiseScene() {
+		declareElements();
+		
+		b.setButtonProperties(noButtonH, "", Menu.WIDTH/2 + onImage.getWidth() + spaceBetweenText, Menu.HEIGHT/3, 
+				e->ElementsHandler.handle(e), new ImageView(offImage));
+		b.addHoverEffect(noButtonH, offImageHovered, offImage, Menu.WIDTH/2 + onImage.getWidth() + spaceBetweenText, Menu.HEIGHT/3);
+		
+		b.setButtonProperties(noButtonS, "", Menu.WIDTH/2 + onImage.getWidth() + spaceBetweenText, Menu.HEIGHT/3 + spaceBetweenImgH, 
+				e->ElementsHandler.handle(e), new ImageView(offImage));
+		b.addHoverEffect(noButtonS, offImageHovered, offImage, Menu.WIDTH/2 + onImage.getWidth() + spaceBetweenText, Menu.HEIGHT/3 + spaceBetweenImgH);
+		
+		b.setButtonProperties(backButton, "", Menu.WIDTH/2 - backImage.getWidth() - spaceBetweenText*4, Menu.HEIGHT/1.1,
+				e->ElementsHandler.handle(e), new ImageView(backImage));
+		b.addHoverEffect(backButton, backImageHovered, backImage, Menu.WIDTH/2 - backImage.getWidth() - spaceBetweenText*4, Menu.HEIGHT/1.1);
+		
+		l.setLabelProperties(hintsLabel, "", Menu.WIDTH/2 - hintsImage.getWidth() - spaceBetweenText, Menu.HEIGHT/3, new ImageView(hintsImage));
+		l.setLabelProperties(soundLabel, "", Menu.WIDTH/2 - soundImage.getWidth() - spaceBetweenText, Menu.HEIGHT/3 + spaceBetweenImgH, new ImageView(soundImage));
+		
+		// To do sound configurations for button as well
+		optionsMenuPane.getChildren().addAll(hintsLabel, soundLabel, noButtonH, noButtonS, backButton);
+		final Group group = new Group(optionsMenuPane);
+		
+		optionsMenuScene = new Scene(group, Menu.WIDTH, Menu.HEIGHT, Color.WHITE);
+		optionsMenuScene.setOnKeyPressed(e->ElementsHandler.handleKeys(e));
+	}
+	
+	/**
+	 * Gets the scene of the options menu
+	 * @return - the scene of the options menu
+	 */
+	public Scene getScene() {
+		return optionsMenuScene;
+	}
+}
+
