7c1413192c5b81604d2931fdbb15a6d7e68c0ee7
Saif Azmi <saif.azmi0108@gmail.com>
2016-02-26 17:07:44 +0000

    Configured test requirements and added the first test class

---
+++ src/test/DepthFirstSearchTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author : saif
+ * @project : bestRTS
+ * @date : 26/02/16
+ */
+public class DepthFirstSearchTest {
+
+    @Test
+    public void testFindPathFrom() throws Exception {
+
+    }
+
+    @Test
+    public void testReturnVisited() throws Exception {
+
+    }
+}
