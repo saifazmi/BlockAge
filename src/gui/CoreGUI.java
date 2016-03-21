454c7c39f072611e83ae24d4540e90559d97c668
Paul Popa <paul.popa1995@yahoo.ro>
2016-03-21 17:17:23 +0000

    nothing

--- src/gui/CoreGUI.java
+++ src/gui/CoreGUI.java
@@ -3,6 +3,7 @@ package gui;
 import core.CoreEngine;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import menus.MenuHandler;
 
 import java.util.logging.Level;
